bfeefe0802a04206dd336cf52cf7ab3fcb89a66a
Ashimpaudel <ashim.paudel@bison.howard.edu>
2017-02-04 18:48:00 -0500

    1. error in non numeric character + comments

--- lp.pl
+++ lp.pl
@@ -1,8 +1,9 @@
-sum-up-numbers-simple([],0). %ase-statement, if the list is empty its sum is 0
+sum-up-numbers-simple([],0). %base-statement, if the list is empty its sum is 0
 sum-up-numbers-simple([X|Y], Z) :-
 
-	%#taking the logic from reverse function given in slides
-	sum-up-numbers-simple(Y, W), %#if W is the sum of Y
+	%taking the logic from reverse function given in slides
+	number(X),
+	sum-up-numbers-simple(Y, W), %if W is the sum of Y
 	Z is X + W.%#adding x to W generates Z 
-	%#then Z is the sum of [X|Y]
+	%then Z is the sum of [X|Y]
 
