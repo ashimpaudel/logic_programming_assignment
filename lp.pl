9396e0bf5dba35418c5d9a12f1a26e8fe8c8f008
Ashimpaudel <ashim.paudel@bison.howard.edu>
2017-02-04 19:07:10 -0500

    1. use of not function to check wehter it is number or not

--- lp.pl
+++ lp.pl
@@ -1,9 +1,11 @@
+sum-up-numbers-simple(not(number), 0).
 sum-up-numbers-simple([],0). %base-statement, if the list is empty its sum is 0
 sum-up-numbers-simple([X|Y], Z) :-
 
 	%taking the logic from reverse function given in slides
-	number(X),
+	%number(X),
 	sum-up-numbers-simple(Y, W), %if W is the sum of Y
+	%number(X),
 	Z is X + W.%#adding x to W generates Z 
 	%then Z is the sum of [X|Y]
 
