256817e395077bd100443a696ab6d9a5ba406322
Ashimpaudel <ashim.paudel@bison.howard.edu>
2017-02-05 16:25:08 -0500

    2. logic of number 2, simply replacing simple with general

--- lp.pl
+++ lp.pl
@@ -1,7 +1,9 @@
+%1. sum-up-numbers-simple(L, N)
+
 sum-up-numbers-simple(X, 0) :-
 	not(number(X)).
 
-sum-up-numbers-simple(X, X):-
+sum-up-numbers-simple(X, X):- %
 	number(X).
 
 %write a function which returns 0 if not number
@@ -13,10 +15,35 @@ sum-up-numbers-simple([],0). %base-statement, if the list is empty its sum is 0
 sum-up-numbers-simple([X|Y], Z) :-
 
 	%taking the logic from reverse function given in slides
-	sum-up-numbers-simple(X, P),
-	sum-up-numbers-simple(Y, W), %if W is the sum of Y
-	%Z is 0 not(number(X));
-	Z is P + W.%#adding x to W generates Z 
-	%then Z is the sum of [X|Y]
 
-%failed-test case = [100,a] 100 True
+	sum-up-numbers-simple(X, P),
+	sum-up-numbers-simple(Y, W), 
+	Z is P + W.
+
+	%if W is the sum of Y then Z is the sum of [X|Y]
+
+
+%2. sum-up-numbers-general(L, N)
+
+
+
+sum-up-numbers-general(X, 0) :-
+	not(number(X)).
+
+sum-up-numbers-general(X, X):- %
+	number(X).
+
+sum-up-numbers-general([],0).
+
+
+
+
+
+sum-up-numbers-general([X|Y], Z):-
+	sum-up-numbers-general(X, P),
+	sum-up-numbers-general(Y, W),
+	Z is P + W.
+
+
+
+
