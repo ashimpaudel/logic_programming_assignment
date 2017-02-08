d74b8e2cc04bfa472d098ef648ff11ec2d0af4a6
Ashimpaudel <ashim.paudel@bison.howard.edu>
2017-02-08 00:47:59 -0500

    4. comments

--- lp.pl
+++ lp.pl
@@ -32,8 +32,10 @@ sum-up-numbers-simple([X|Y], Z) :-
 
 	%if W is the sum of Y then Z is the sum of [X|Y]
 
+%if not number, sum_helper returns 0 whereas when number sum_helper return the number itself
 sum_helper(X, 0):-
 	not(number(X)).
+
 sum_helper(X, X):-
 	number(X).
 
@@ -72,14 +74,15 @@ sum-up-numbers-general([X|Y], Z) :-
 
 
 %3. predicate min-above-min(L1, L2, N)
+
 %using the same logic in assignmet 1
+
 %minimum predicate
 
 min(X, Y, X) :- X < Y.
 min(X, Y, Y) :- X >= Y.
 
 %finding minimum item of a list
-%find_min_list([], 999999). base case
 
 find_min_list([],[]). %if list is null return null
 
@@ -89,6 +92,7 @@ find_min_list(L1, X) :-
 	not(number(A)),
 	find_min_list(B,X).
 
+%if the minimum of rest of list is not an number
 find_min_list(L1, X) :-
 	[A|B] = L1,
 	number(A),
@@ -97,7 +101,7 @@ find_min_list(L1, X) :-
 	X is A.
 
 
-
+%base-case minimum of list with 1 element is the element itself
 find_min_list(X, Y):-
 	length(X,1),
 	[A|B] = X,
@@ -105,7 +109,7 @@ find_min_list(X, Y):-
 	Y is A.
 
 
-
+%very general case of minimum, find the minimum of first item and rest item
 find_min_list(L1, N):-
 	[A|B] = L1,	
 	number(A),
@@ -113,14 +117,14 @@ find_min_list(L1, N):-
 	number(N1),
 	min(A, N1, N).
 
-
+%if minimum of rest of the list is not found, minimum of first item is its minimum
 find_min_list(L1, N):-
 	[A|B] = L1,	
 	number(A),
 	not(find_min_list(B, N1)),
 	N is A.
 
-
+%it returns number only when X is greater than Y
 compare_special(X, Y, X) :-
 	number(X),
 	number(Y),
@@ -130,13 +134,13 @@ compare_special(X, Y, X) :-
 
 
 %base cases for min-above-min
-%min-above-min([], L2, 9999999999). % when L1 is empty, return False
+
+%when L2 is empty, return the minimum of L1
 
 min-above-min(L1, [], X):-
-	find_min_list(L1, X). %when L2 is empty, return the minimum of L1
+	find_min_list(L1, X). 
 
 %min-above-min function when there is only 1 item in the L1
-
 min-above-min(L1, L2, X) :-
 	[A|B] = L1,
 	length(B,0),
@@ -144,17 +148,15 @@ min-above-min(L1, L2, X) :-
 	compare_special(A, Y, X).
 
 %handles the case where CDR of L1 dont have any numeric character
-
 min-above-min(L1, L2, X) :-
 	[A|B] = L1,
 	find_min_list(L2, P), %P is the minimum item of L2
 	compare_special(A,P,Z), %comparing first item of L1 with P will result to Z
 	not(min-above-min(B, L2, A2)),
 	X is A.
-	%min-above-min(B, L2, A1),
-	%min(A1, Z, X).
+	
 
-%If CAR L1 is less compare to value of min-above-min of CDR L1 return CAR L1
+%If first item of  L1 is less compare to value of min-above-min of rest of the item L1, returns first item
 min-above-min(L1, L2, X):-
 	[A|B] = L1,
 	find_min_list(L2, P), %P is the minimum item of L2
@@ -164,7 +166,7 @@ min-above-min(L1, L2, X):-
 
 	
 	
-%else return min-above-min of CDR of L1
+%else returns min-above-min of the rest of the list
 min-above-min(L1, L2, X) :-
 	[A|B] = L1,
 	find_min_list(L2, P), %P is the minimum item of L2
@@ -175,7 +177,7 @@ min-above-min(L1, L2, X) :-
 
 
 
-%else of first loop
+%else iff first conditions fails
 min-above-min(L1, L2, X) :-
 	[A|B] = L1,
 	find_min_list(L2, P), %P is the minimum item of L2
@@ -183,7 +185,7 @@ min-above-min(L1, L2, X) :-
 	min-above-min(B, L2, X).
 
 
-
+%most general case
 min-above-min(L1, L2, X) :-
 	[A|B] = L1,
 	find_min_list(L2, P), %P is the minimum item of L2
@@ -199,6 +201,7 @@ common-unique-elements([],[],[]).
 common-unique-elements(L1,[],[]). %when first list is empty
 common-unique-elements([],L2,[]). %when second list is empty
 
+%if first element of L1 is member of L2
 common-unique-elements(L1, L2, X) :-
 	[A|B] = L1,
 
@@ -207,6 +210,7 @@ common-unique-elements(L1, L2, X) :-
 	common-unique-elements(B, L2, Y),
 	append([A],Y,X).
 
+%if first element is not member of L2
 common-unique-elements(L1, L2, X) :-
 	[A|B] = L1,
 	not(is_list(A)),
@@ -215,12 +219,13 @@ common-unique-elements(L1, L2, X) :-
 	common-unique-elements(B, L2, X).
 	
 %for nested list
-
+%when elements in L1 are nested
 common-unique-elements(L1, L2, X) :-
 	[A|B] = L1,
 	is_list(A),
 	common-unique-elements(A,L2,P).
 
+%when elements in L2 are nested
 common-unique-elements(L1,L2,X):-
 	[C|D] = L2,
 	is_list(C),
