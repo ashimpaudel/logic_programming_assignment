bbc6c8d3497561d5ec7f2a9796d0b20b407f77cf
Ashimpaudel <ashim.paudel@bison.howard.edu>
2017-02-07 19:25:19 -0500

    3. updating cases for min-above-min

--- lp.pl
+++ lp.pl
@@ -70,6 +70,7 @@ sum-up-numbers-general([X|Y], Z) :-
 
 	%if W is the sum of Y then Z is the sum of [X|Y]
 
+
 %3. predicate min-above-min(L1, L2, N)
 %using the same logic in assignmet 1
 %minimum predicate
@@ -78,7 +79,8 @@ min(X, Y, X) :- X < Y.
 min(X, Y, Y) :- X >= Y.
 
 %finding minimum item of a list
-%find_min_list([], 999999). %base case
+%find_min_list([], 999999). base case
+
 find_min_list(X, Y):-
 	length(X,1),
 	[A|B] = X,
@@ -95,13 +97,47 @@ find_min_list(L1, N):-
 	not(number(A)),
 	find_min_list(B,N).
 
-
-%base cases for min-above-min
-min-above-min([], L2, 9999999999). % when L1 is empty, return False
-min-above-min(L1, [], X):-
-	find_min_list(L1, X). %when L2 is empty, return the minimum of L1
-
+compare_special(X, Y, X) :-
+	number(X),
+	number(Y),
+	X > Y.
 
 	
 
+
+%base cases for min-above-min
+%min-above-min([], L2, 9999999999). % when L1 is empty, return False
+
+min-above-min(L1, [], X):-
+	find_min_list(L1, X). %when L2 is empty, return the minimum of L1
+
+%min-above-min function when there is only 1 item in the L1
+
+min-above-min(L1, L2, X) :-
+	[A|B] = L1,
+	length(B,0),
+	find_min_list(L2,Y),
+	compare_special (A, Y, X).
+
+%handles the case where CDR of L1 dont have any numeric character
+
+min-above-min (L1, L2, X) :-
+	[A|B] = L1,
+	find_min_list(L2, P), %P is the minimum item of L2
+	compare_special(A,P,Z), %comparing first item of L1 with P will result to Z
+	not(min-above-min(B, L2, A2)),
+	X is A.
+	%min-above-min(B, L2, A1),
+	%min(A1, Z, X).
+
+min-above-min (L1, L2, X):-
+	
+min-above-min(L1, L2, X) :-
+	[A|B] = L1,
+	find_min_list(L2, P), %P is the minimum item of L2
+	not(compare_special(A,P,Z)), %comparing first item of L1 with P will result to Z
+	min-above-min(B,L2,X).
+
+
+	
 
